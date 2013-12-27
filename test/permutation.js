420b6d9fda4562f06956b5459b2c4fb1ffa45305
Jordan Hill <jormhill@gmail.com>
2013-12-26 21:43:24 -0800

    Added tests for zero-size combinations and permutations.

--- test/permutation.js
+++ test/permutation.js
@@ -15,7 +15,11 @@ var is_deeply = function (a, e, m) {
 
 describe('Combinatorics.permutation', function () {
     var a = 'abcd'.split(''),
-        c = Combinatorics.permutation(a, 1);
+        c = Combinatorics.permutation(a, 0);
+    it([a, 0], is_deeply(c.toArray(), [
+        []
+    ]));
+    c = Combinatorics.permutation(a, 1);
     it([a, 1], is_deeply(c.toArray(), [
         ["a"],
         ["b"],
