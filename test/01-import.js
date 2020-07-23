99efe2bb3feec8395e622685c740fa0ca97f35d5
Dan Kogai <dankogai+github@gmail.com>
2020-07-24 04:36:55 +0900

    add test/02-arithmetics.js

--- test/01-import.js
+++ test/01-import.js
@@ -2,8 +2,6 @@ import * as Combinatorics from '../combinatorics.js';
 describe('import', () => {
     for (const k in Combinatorics) {
         const tn = k === 'version' ? 'string' : 'function';
-        it(`${k} is a ${tn}`, () =>
-           chai.expect(typeof Combinatorics[k]).to.equal(tn)
-          );
+        it(`${k} is a ${tn}`, ()=>chai.expect(typeof Combinatorics[k]).to.equal(tn));
     }
 });
---
+++ test/02-arithmetics.js
@@ -0,0 +1,58 @@
+import {
+    permutation, combination, factorial,factoradic
+} from '../combinatorics.js';
+
+const $$ = chai.expect.bind(chai);
+
+describe('permutation', () => {
+    it('permutation(3,2) === 6', ()=>$$(permutation(3, 2)).to.equal(6));
+    it('permutation(1,1) === 1', ()=>$$(permutation(1, 1)).to.equal(1));
+    it('permutation(2,5) === 0', ()=>$$(permutation(2, 5)).to.equal(0));
+    // it('permutation(5, 1.5) throws RangeError', () => {
+    //     chai.assert.throws(() => combination(5, 1.5), RangeError)
+    // });
+    if (typeof BigInt === 'function') {
+        const sbn = '10333147966386144929666651337523200000000';
+        it(`permutation(35,35) === ${sbn}`, () => {
+            $$(permutation(35,35)).to.equal(BigInt(sbn));
+        });
+    } else {
+        it.skip('SKIP permutation(35,35): BigInt unsupported', x=>x);
+    }
+});
+
+describe('combination', () => {
+    it('combination(3,2) === 3', ()=>$$(combination(3, 2)).to.equal(3));
+    it('combination(1,1) === 1', ()=>$$(combination(1, 1)).to.equal(1));
+    it('combination(2,5) === 0', ()=>$$(combination(2, 5)).to.equal(0));
+    // it('combination(5, 1.5) throw RangeError', () => {
+    //     chai.assert.throws(() => combination(5, 1.5), RangeError);
+    // });
+    if (typeof BigInt === 'function') {
+        const sbn = '23951146041928082866135587776380551750';
+        it(`combination(128,64) === ${sbn}`, () => {
+            $$(combination(128,64)).to.equal(BigInt(sbn));
+        });
+    } else {
+        it.skip('SKIP combination(128,64): BigInt unsupported', x=>x);
+    }
+});
+
+describe('factoradic', () => {
+    const count = function*(n) {
+        let i = 0; while (i < n) yield i++;
+    };
+    for (let i of count(32)) {
+        let n = factorial(i);
+        if (Number.MAX_SAFE_INTEGER <= --n) {
+            if (typeof BigInt !== 'function') {
+                it.skip(`SKIP factoradic(${i}! - 1): BigInt unsupported`, x=>x);
+                break;
+            }
+        }
+        const a = i == 0 ? [0] : [...count(i)];
+        it(`factoradic(${i}! - 1)`, () => 
+            $$(factoradic(n)).to.deep.equal(a)
+        );
+    }
+});
