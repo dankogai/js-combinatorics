6bba79d6f6c80d893ef2a0b5486eb9c0dad8ccbe
Dan Kogai <dankogai+github@gmail.com>
2020-07-24 01:30:04 +0900

    start adding tests again

---
+++ test/01-import.js
@@ -0,0 +1,9 @@
+import * as Combinatorics from '../combinatorics.js';
+describe('import', () => {
+    for (const k in Combinatorics) {
+        const tn = k === 'version' ? 'string' : 'function';
+        it(`${k} is a ${tn}`, () =>
+           chai.expect(typeof Combinatorics[k]).to.equal(tn)
+          );
+    }
+});
